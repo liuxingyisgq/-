b8dacdfcf2df1282a601b4787a16200ef9eb252f
liuxingyisgq <131181324+liuxingyisgq@users.noreply.github.com>
2023-06-18 00:19:53 +0800

    Delete back directory

--- back/src/main/java/org/example/mapper/StudentMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.example.pojo.Student;
-
-public interface StudentMapper extends BaseMapper<Student> {
-}
